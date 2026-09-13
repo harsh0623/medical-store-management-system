f09d1817211d8367671fa5c1796a476e19677e43
harsh <hv3445393@gmail.com>
2026-09-13 14:40:40 +0530

    feat: initialize Spring Boot backend

---
+++ src/main/java/com/medicalstore/MedicalStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medicalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalStoreBackendApplication.class, args);
+	}
+
+}
